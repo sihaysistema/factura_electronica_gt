dd70987009f5acea1107106d62781be9dca0a769
Retina <retina@Retina-MacBook-Pro-AB.local>
2017-11-11 08:58:15 -0600

    es-gt: Agregando comentario para documentacion EN-US:  adding comments for documentation

--- factura_electronica/config/docs.py
+++ factura_electronica/config/docs.py
@@ -4,8 +4,8 @@ Configuration for docs
 
 # source_link = "https://github.com/[org_name]/factura_electronica"
 # docs_base_url = "https://[org_name].github.io/factura_electronica"
-# headline = "App that does everything"
-# sub_heading = "Yes, you got that right the first time, everything"
+# headline = "Factura Electronica"
+# sub_heading = "Envio de factura electronica directo desde ERPNext"
 
 def get_context(context):
 	context.brand_html = "Factura Electronica"
--- factura_electronica/factura_electronica/doctype/configuracion_factura_electronica/configuracion_factura_electronica.js
+++ factura_electronica/factura_electronica/doctype/configuracion_factura_electronica/configuracion_factura_electronica.js
@@ -2,12 +2,21 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Configuracion Factura Electronica', {
-    refresh: function(frm) {
-
+    // en-US # Upon form refresh
+	// es-GT # Al refrescar el formulario
+	refresh: function(frm) {
+    // en-US # Call the python method indicated
+	// es-GT # Llame al modulo python indicado
         frappe.call({
+    		// en-US # Call the series_sales_invoice  method.
+			// es-GT # Llame al metodo  series_sales_invoice.
             method: "factura_electronica.factura_electronica.doctype.configuracion_factura_electronica.configuracion_factura_electronica.series_sales_invoice",
-            callback: function(r) {
+    		// en-US # Obtains the field 'serie' from the Configuracion Series DocType
+			// es-GT # Obtiene el campo 'serie' del DocType "Configuracion Series"
+			callback: function(r) {
                 frappe.meta.get_docfield('Configuracion Series', 'serie', cur_frm.doc.name).options = r.message
+    			// en-US # Updates the current form fiel 'serie' with the previously obtained data
+				// es-GT # Actualiza el campo 'serie' del formulario actual, con la data obtenida anteriormente
                 cur_frm.refresh_field('serie');
             }
         });
