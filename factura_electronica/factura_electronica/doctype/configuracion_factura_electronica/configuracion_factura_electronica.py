87a5eec1604aabbd98807ff59e7152c5498ea784
Tropicalrambler <alainber@gmail.com>
2017-11-11 14:03:35 -0600

    Agregando comentarios de uso

--- factura_electronica/factura_electronica/doctype/configuracion_factura_electronica/configuracion_factura_electronica.py
+++ factura_electronica/factura_electronica/doctype/configuracion_factura_electronica/configuracion_factura_electronica.py
@@ -13,7 +13,12 @@ sys.setdefaultencoding('utf-8')
 
 class ConfiguracionFacturaElectronica(Document):
 	pass
-
+# es-GT: Se hace whitelist o abre permiso para permitir llamados Asincronicos desde el navegador del usuario
+# en-US: Whitelisting the function to permit Asyncrhonous calls from the user's web browser
+# es-GT: Esta funcion obtiene el contenido del campo "Naming Series" configurada por el usuario para "Sale Invoice". (Las series configuradas por el usuario para Facturas de Venta)
+# es-GT: Luego de obtenerlas, se separan las series por cada linea, usando el indicador de escape de nueva linea: "\n"
+# en-US: This function obtains the contents of "Naming Series" field for "Sales Invoice", a field which lists the series configured by the user.
+# en-US: After obtaining them, the series are separated by each new line, using the new line excape character "\n"
 @frappe.whitelist()
 def series_sales_invoice():
 	naming_series = frappe.get_meta("Sales Invoice").get_field("naming_series").options or ""
