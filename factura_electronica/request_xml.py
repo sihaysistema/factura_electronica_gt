fe16c18bfe1576630313cce3a1fa368d2eff214c
monroy95 <m.monroyc22@gmail.com>
2017-12-29 16:55:09 -0600

    Agregando Fix 2 a Calculos

--- factura_electronica/request_xml.py
+++ factura_electronica/request_xml.py
@@ -109,7 +109,7 @@ def construir_xml(sales_invoice, direccion_cliente, datos_cliente, sales_invoice
 
 				# montoBrutoTag_Value =  float(sales_invoice_item[i]['net_amount']) ((Valor /(1+Tasa_IVA))
 				# FIXME: Mejor opcion, obtener el valor del iva directo de la tabla de DB
-				montoBrutoTag_Value =  float(((sales_invoice_item[i]['gt_tax_net_fuel_amt'])+(sales_invoice_item[i]['gt_tax_net_goods_amt'])+ (sales_invoice_item[i]['gt_tax_net_services_amt']))/1.12)
+				montoBrutoTag_Value =  '{0:.2f}'.format(float(((sales_invoice_item[i]['gt_tax_net_fuel_amt'])+(sales_invoice_item[i]['gt_tax_net_goods_amt'])+ (sales_invoice_item[i]['gt_tax_net_services_amt']))/1.12))
 
 				# es-GT: Calculo de IVA segun requiere infile.
 				# en-US: IVA calculation as required by infile.
@@ -163,7 +163,7 @@ def construir_xml(sales_invoice, direccion_cliente, datos_cliente, sales_invoice
 		
 		# montoBrutoTag_Value =  float(sales_invoice_item[0]['net_amount'])
 		# FIXME: Mejor opcion, obtener el valor del iva directo de la tabla de DB
-		montoBrutoTag_Value =  float(((sales_invoice_item[0]['gt_tax_net_fuel_amt'])+(sales_invoice_item[0]['gt_tax_net_goods_amt'])+ (sales_invoice_item[0]['gt_tax_net_services_amt']))/1.12)
+		montoBrutoTag_Value =  '{0:.2f}'.format(float(((sales_invoice_item[0]['gt_tax_net_fuel_amt'])+(sales_invoice_item[0]['gt_tax_net_goods_amt'])+ (sales_invoice_item[0]['gt_tax_net_services_amt']))/1.12))
 
 		# es-GT: Calculo de IVA segun requiere infile.
 		# en-US: IVA calculation as required by infile.
