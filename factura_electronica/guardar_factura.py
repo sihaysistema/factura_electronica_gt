370b5b54df218f0b1d5f5a85a7a149bed87a6447
monroy95 <m.monroyc22@gmail.com>
2018-05-18 18:17:37 -0600

    Modificacion en Envios Facturas Electronicas

--- factura_electronica/guardar_factura.py
+++ factura_electronica/guardar_factura.py
@@ -25,7 +25,7 @@ def guardar_factura_electronica(datos_recibidos, serie_fact, tiempo_envio):
 
         # en-US: documento: with the xmltodict library, it is converted from XML to Dictionary, to access the data through the dictionary keys
         # en-US: All this is assigned to the 'document' variable.
-        documento = xmltodict.parse(datos_recibidos)
+        respuestaINFILE = xmltodict.parse(datos_recibidos)
 
         # es-GT: Crea un nuevo record de Envios Facturas Electronica en la base de datos.
         # en-US: Creates a new Electronic Invoice Sent record in the database
@@ -37,44 +37,46 @@ def guardar_factura_electronica(datos_recibidos, serie_fact, tiempo_envio):
 
         # es-GT: Obtiene y Guarda el CAE (Codigo de Autorización Electronico)
         # en-US: Obtains and Saves the CAE or literally translated: "Code of Electronic Authorization"
-        tabFacturaElectronica.cae = str(documento['S:Envelope']['S:Body']['ns2:registrarDteResponse']['return']['cae'])
-        cae_dato = str(documento['S:Envelope']['S:Body']['ns2:registrarDteResponse']['return']['cae'])
+        tabFacturaElectronica.cae = str(respuestaINFILE['S:Envelope']['S:Body']['ns2:registrarDteResponse']['return']['cae'])
+        cae_dato = str(respuestaINFILE['S:Envelope']['S:Body']['ns2:registrarDteResponse']['return']['cae'])
 
         # es-GT: Obtiene y Guarda el Numero de Documento que quedo registrada ante el Generador de Factura Electronica y la SAT
         # en-US: Obtains and Saves the Document Number that was registered with the Electronic Invoice Generator and SAT
-        tabFacturaElectronica.numero_documento = str(documento['S:Envelope']['S:Body']['ns2:registrarDteResponse']['return']['numeroDocumento'])
+        tabFacturaElectronica.numero_documento = str(respuestaINFILE['S:Envelope']['S:Body']['ns2:registrarDteResponse']['return']['numeroDocumento'])
 
         # es-GT: Obtiene y Guarda el Estado según GFACE (GFACE = Generador de Factura Electronica)
         # en-US: Obtains and Saves the current State of the document as per GFACE (Electronic Invoice Generator)
-        tabFacturaElectronica.estado = str(documento['S:Envelope']['S:Body']['ns2:registrarDteResponse']['return']['estado'])
+        tabFacturaElectronica.estado = str(respuestaINFILE['S:Envelope']['S:Body']['ns2:registrarDteResponse']['return']['estado'])
 
         # es-GT: Obtiene y Guarda las Anotaciones segun GFACE
         # en-US: Obtains and Saves the Annotations as per GFACE
-        tabFacturaElectronica.anotaciones = str(documento['S:Envelope']['S:Body']['ns2:registrarDteResponse']['return']['anotaciones'])
+        tabFacturaElectronica.anotaciones = str(respuestaINFILE['S:Envelope']['S:Body']['ns2:registrarDteResponse']['return']['anotaciones'])
 
         # es-GT: Obtiene y Guarda la Descripcion: En este campo estan todas las descripciones de errores y mensaje de generacion correcta
         # en-US: Obtains and Saves the Description: This field contains all the descriptions of errors and correct generation messages
-        tabFacturaElectronica.descripcion = str(documento['S:Envelope']['S:Body']['ns2:registrarDteResponse']['return']['descripcion'])
+        tabFacturaElectronica.descripcion = str(respuestaINFILE['S:Envelope']['S:Body']['ns2:registrarDteResponse']['return']['descripcion'])
 
         # es-GT: Obtiene y Guarda la Validez del documento
         # en-US: Obtains and Saves the Validity state of the document
-        tabFacturaElectronica.valido = str(documento['S:Envelope']['S:Body']['ns2:registrarDteResponse']['return']['valido'])
+        tabFacturaElectronica.valido = str(respuestaINFILE['S:Envelope']['S:Body']['ns2:registrarDteResponse']['return']['valido'])
 
         # es-GT: Obtiene y Guarda el Numero DTE
         # en-US: Obtains and Saves the DTE Number
-        tabFacturaElectronica.numero_dte = str(documento['S:Envelope']['S:Body']['ns2:registrarDteResponse']['return']['numeroDte'])
+        tabFacturaElectronica.numero_dte = str(respuestaINFILE['S:Envelope']['S:Body']['ns2:registrarDteResponse']['return']['numeroDte'])
+        # numeroDTEINFILE: Guarda el numero DTE, sera utilizado para renombrar la serie original de la factura que lo origino
+        numeroDTEINFILE = str(respuestaINFILE['S:Envelope']['S:Body']['ns2:registrarDteResponse']['return']['numeroDte'])
 
         # es-GT: Obtiene y Guarda el Rango Final Autorizado
         # en-US: Obtains and Saves the Authorized Final Range
-        tabFacturaElectronica.rango_final_autorizado = str(documento['S:Envelope']['S:Body']['ns2:registrarDteResponse']['return']['rangoFinalAutorizado'])
+        tabFacturaElectronica.rango_final_autorizado = str(respuestaINFILE['S:Envelope']['S:Body']['ns2:registrarDteResponse']['return']['rangoFinalAutorizado'])
 
         # es-GT: Obtiene y Guarda el Rango Inicial Autorizado
         # en-US: Obtains and Saves the Initial Authorized Range
-        tabFacturaElectronica.rango_inicial_autorizado = str(documento['S:Envelope']['S:Body']['ns2:registrarDteResponse']['return']['rangoInicialAutorizado'])
+        tabFacturaElectronica.rango_inicial_autorizado = str(respuestaINFILE['S:Envelope']['S:Body']['ns2:registrarDteResponse']['return']['rangoInicialAutorizado'])
 
         # es-GT: Obtiene y Guarda el Regimen de impuestos
         # en-US: Obtains and Saves the Legal Tax Structure, aka 'Regimen'
-        tabFacturaElectronica.regimen = str(documento['S:Envelope']['S:Body']['ns2:registrarDteResponse']['return']['regimen'])
+        tabFacturaElectronica.regimen = str(respuestaINFILE['S:Envelope']['S:Body']['ns2:registrarDteResponse']['return']['regimen'])
 
         # es-GT: Obtiene y Guarda el tiempo en que se recibieron los datos de INFILE
         # en-US: Obtains and Saves the timestamp of data reception from INFILE
@@ -91,6 +93,27 @@ def guardar_factura_electronica(datos_recibidos, serie_fact, tiempo_envio):
         tabFacturaElectronica.save()
         # es-GT: Al terminar de guardar el registro en la base de datos, retorna el CAE
         # en-US: When done saving the records to the database, it returns de CAE
+
+        # EN DASARROLLO #############################################################
+        # try:
+        #     # serieDte: guarda el numero DTE retornado por INFILE, se utilizara para reemplazar el nombre de la serie de la
+        #     # factura que lo generó.
+        #     serieDte = numeroDTEINFILE
+        #     # serieFacturaOriginal: Guarda la serie original de la factura.
+        #     serieFacturaOriginal = serie_fact
+
+        #     # frappe.db.sql("""update `tabSales Invoice` set name = '{0}'
+        #     # where name = '{1}'""").format(serieDte, serie_fact)
+
+        #     # QUERY para actualizar la serie de la factura que lo origino, con el numero de DTE
+        #     # Listado tablas para actualizar.
+        #     frappe.db.sql('''UPDATE `tabSales Invoice` SET name=%(name)s WHERE name=%(serieFa)s''', {'name':serieDte, 'serieFa':serieFacturaOriginal})
+        #     frappe.db.sql('''UPDATE `tabSales Invoice Item` SET parent=%(name)s WHERE parent=%(serieFa)s''', {'name':serieDte, 'serieFa':serieFacturaOriginal})
+        #     frappe.db.sql('''UPDATE `tabGL Entry` SET voucher_no=%(name)s, against_voucher=%(name)s WHERE voucher_no=%(serieFa)s''', {'name':serieDte, 'serieFa':serieFacturaOriginal})
+        # except:
+        #     # En caso exista un error al renombrar la factura retornara el mensaje con el error
+        #     frappe.msgprint(_('Error al renombrar Factura. Por favor intente de nuevo presionando el boton Factura Electronica'))
+
         return cae_dato
         # frappe.msgprint(_("Factura Electronica Generada!"))
     except:
