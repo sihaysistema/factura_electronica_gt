fe12cf96a47181fcfb41db2a73bea184be8bc5f8
monroy95 <m.monroyc22@gmail.com>
2018-08-29 17:35:25 -0600

    Arreglo setup.py compatibilidad pip

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+# from pip.req import parse_requirements
 import re, ast
+
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
 
 # get version from __version__ variable in factura_electronica/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
@@ -10,7 +13,7 @@ with open('factura_electronica/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
-requirements = parse_requirements("requirements.txt", session="")
+# requirements = parse_requirements("requirements.txt", session="")
 
 setup(
 	name='factura_electronica',
@@ -21,6 +24,6 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	install_requires=install_requires
+	# dependency_links=[str(ir._link) for ir in requirements if ir._link]
 )
