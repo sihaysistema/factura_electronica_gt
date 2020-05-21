3846fe8d2e94352c77d83adc30ba95a6b4a80f54
monroy95 <m.monroyc22@gmail.com>
2020-05-21 18:36:47 +0000

    #115. 2, 3, 4 Agregando base para botones

--- factura_electronica/factura_electronica/doctype/batch_electronic_invoice/batch_electronic_invoice.js
+++ factura_electronica/factura_electronica/doctype/batch_electronic_invoice/batch_electronic_invoice.js
@@ -2,7 +2,36 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Batch Electronic Invoice', {
-	// refresh: function(frm) {
+    refresh: function (frm) {
 
-	// }
+        // frm.set_intro(__(
+        //     'NOTA PARA GENERADOR DE LOTES'
+        // ));
+        // Validador de funcion, que muestra el boton de import payments SOLAMENTE despues de Guardado.
+
+        // Agrega clase bootstrap al boton
+        frm.get_field("validate_invoices").$input.addClass("btn btn-primary");
+
+
+        fel_generator(frm);
+    }
 });
+
+
+function fel_generator(frm) {
+    frm.add_custom_button(__("Generate Electronic Invoice"), function () {
+        // frappe.call({
+        //     method: "factura_electronica.batch_api.test_function",
+        //     args: {
+        //         references: frm.doc.expenses,
+        //         docname: frm.doc.name,
+        //         compa: frm.doc.company,
+        //         posting_date: frm.doc.posting_date,
+        //         je_reference: frm.doc.journal_entry_reference || "",
+        //     },
+        //     callback: function (r) {
+        //         frm.reload_doc();
+        //     },
+        // });
+    }).addClass("btn-primary");
+}
