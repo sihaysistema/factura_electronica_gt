bd6af47a74d156092e9ea9db7095f255eeb7932d
ronaldoramirez <perencen17@gmail.com>
2020-07-01 21:44:35 +0000

    #127 Creando Reporte

---
+++ factura_electronica/factura_electronica/report/vat_and_income_tax_retention_report/vat_and_income_tax_retention_report.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, Frappe and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["VAT and Income Tax Retention Report"] = {
+	"filters": [
+
+	]
+};
