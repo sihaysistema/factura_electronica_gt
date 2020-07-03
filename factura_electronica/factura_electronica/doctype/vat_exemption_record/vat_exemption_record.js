b071f66ed7829302a2d44784cfb0fd2d8161dee9
Tropicalrambler <alainber@gmail.com>
2020-07-03 20:01:08 +0000

    #138 new doctypes sans translations

---
+++ factura_electronica/factura_electronica/doctype/vat_exemption_record/vat_exemption_record.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Frappe and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('VAT Exemption Record', {
+	// refresh: function(frm) {
+
+	// }
+});
