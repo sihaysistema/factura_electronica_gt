6d2fdb4e6933c869f85d7e976de6687f19a8e052
Tropicalrambler <alainber@gmail.com>
2020-07-09 18:32:56 +0000

    pulling gl entry queries succesfully

--- factura_electronica/factura_electronica/report/vat_payable_and_receivable_conciliation/vat_payable_and_receivable_conciliation.py
+++ factura_electronica/factura_electronica/report/vat_payable_and_receivable_conciliation/vat_payable_and_receivable_conciliation.py
@@ -51,24 +51,27 @@ def get_columns():
             "label": _("Transaction Date"),
             "fieldname": "trans_date",
             "fieldtype": "Date",
+            "width": 150
+        },
+        {
+            "label": _("VAT Debit"),
+            "fieldname": "vat_debit",
+            "fieldtype": "Currency",
+            "options": "currency",
             "width": 200
         },
         {
-            "label": _("Net Total (if invoice)"),
-            "fieldname": "net_total",
-            "fieldtype": "Data",
-            "width": 200
-        },
-        {
-            "label": _("VAT Amount"),
-            "fieldname": "vat_amount",
-            "fieldtype": "Data",
+            "label": _("VAT Credit"),
+            "fieldname": "vat_credit",
+            "fieldtype": "Currency",
+            "options": "currency",
             "width": 200
         },
         {
             "label": _("Transaction Total"),
             "fieldname": "trans_total",
-            "fieldtype": "Data",
+            "fieldtype": "Currency",
+            "options": "currency",
             "width": 200
         },
         {
@@ -87,11 +90,10 @@ def get_data(filters):
     data = [empty_row]
     initial_vat_payable = {
         "doc_type": "",
-        # "doc_id": "<strong>Saldo Inicial IVA por pagar</strong>",
         "doc_id": "<strong>Saldo Inicial IVA por pagar</strong>",
         "trans_date": "",
-        "net_total": "",
-        "vat_amount": "300.00",
+        "vat_debit": "",
+        "vat_credit": "300.00",
         "trans_total": "",
         "currency": "GTQ"
     }
@@ -99,8 +101,8 @@ def get_data(filters):
         "doc_type": "",
         "doc_id": "<strong>IVA POR PAGAR</strong>",
         "trans_date": "",
-        "net_total": "",
-        "vat_amount": "",
+        "vat_debit": "",
+        "vat_credit": "",
         "trans_total": "",
         "currency": "GTQ"
     }
@@ -108,8 +110,8 @@ def get_data(filters):
         "doc_type": "",
         "doc_id": "<strong>SUBTOTAL IVA POR PAGAR</strong>",
         "trans_date": "",
-        "net_total": "",
-        "vat_amount": "672.00",
+        "vat_debit": "",
+        "vat_credit": "672.00",
         "trans_total": "",
         "currency": "GTQ"
     }
@@ -117,8 +119,8 @@ def get_data(filters):
         "doc_type": "",
         "doc_id": "<strong>IVA POR COBRAR</strong>",
         "trans_date": "",
-        "net_total": "",
-        "vat_amount": "",
+        "vat_debit": "",
+        "vat_credit": "",
         "trans_total": "",
         "currency": "GTQ"
     }
@@ -126,8 +128,8 @@ def get_data(filters):
         "doc_type": "",
         "doc_id": "<strong>SUBTOTAL IVA POR COBRAR</strong>",
         "trans_date": "",
-        "net_total": "",
-        "vat_amount": "36.00",
+        "vat_debit": "",
+        "vat_credit": "36.00",
         "trans_total": "",
         "currency": "GTQ"
     }
@@ -135,8 +137,8 @@ def get_data(filters):
         "doc_type": "",
         "doc_id": "<strong>IVA a liquidar este mes</strong>",
         "trans_date": "",
-        "net_total": "",
-        "vat_amount": "636.00",
+        "vat_debit": "",
+        "vat_credit": "636.00",
         "trans_total": "",
         "currency": "GTQ"
     }
@@ -144,20 +146,31 @@ def get_data(filters):
         "doc_type": "",
         "doc_id": "<strong>Monto a Liquidar Incluyendo Saldos pendientes</strong>",
         "trans_date": "",
-        "net_total": "",
-        "vat_amount": "936.00",
+        "vat_debit": "",
+        "vat_credit": "936.00",
         "trans_total": "",
         "currency": "GTQ"
     }
     data.append(initial_vat_payable)
     data.append(por_pagar_header)
-    por_pagar = get_vat_payable_data(filters)
+
+    # en_US: Getting the transactions for vat payable accounts to insert the rows, for this month.
+    # es: Obtenemos las transacciones de IVA por pagar de este mes para insertar las filas.
+
+    por_pagar = apply_on_site_links(get_vat_payable_data(filters))
     data.extend(por_pagar)
+
     data.append(por_pagar_footer)
     data.append(empty_row)
     data.append(empty_row)
     data.append(por_cobrar_header)
-    data.append(empty_row)
+
+
+    # en_US: Getting the transactions for vat receivable accounts to insert the rows, for this month.
+    # es: Obtenemos las transacciones de IVA por cobrar de este mes para insertar las filas.
+    por_cobrar = apply_on_site_links(get_vat_receivable_data(filters))
+    data.extend(por_cobrar)
+
     data.append(por_cobrar_footer)
     data.append(empty_row)
     data.append(empty_row)
@@ -167,4 +180,4 @@ def get_data(filters):
     return data
 
 # Generate links to documents found.
-# link_ref = f'https://{site_erp}/desk#Form/Payment%20Entry/{ref_per}'
+
