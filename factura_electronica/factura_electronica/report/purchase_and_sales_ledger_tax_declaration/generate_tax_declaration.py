9a26362fe270520ef34ef7bb406fb314f1ae9cd5
Tropicalrambler <alainber@gmail.com>
2020-07-18 00:58:18 +0000

    #169 saving a new vat declaration

--- factura_electronica/factura_electronica/report/purchase_and_sales_ledger_tax_declaration/generate_tax_declaration.py
+++ factura_electronica/factura_electronica/report/purchase_and_sales_ledger_tax_declaration/generate_tax_declaration.py
@@ -7,8 +7,12 @@ from datetime import datetime, date, time
 import os
 
 @frappe.whitelist()
-def generate_vat_declaration(filters, report_data):
+def generate_vat_declaration(company, year, month, declared):
     try:
+           # Cargar report_data con json load.
+        # en_US: Loads the received data as if it was a json into a dictionary
+        # es: Carga como json-diccionario la data recibida
+        # documents = json.loads(report_data)
         # TODO
         # Crear Vat declaration
         # Opcion 1:
@@ -16,30 +20,36 @@ def generate_vat_declaration(filters, report_data):
         # CON PY:  De lo contrario, obviamos lo de arriba y solo trabajamos lo de abajo:
         # Necesitamos filters del report, pasados como argumentos desde el frappe.call: declared , company, month, year, [] 
         #  validaciones:
-        if filters.declared == 'Not Declared':
-            # Generate doctype for vat declaration
-            this_dec = frappe.get_doc("VAT Declaration")
-            this_dec.title = "Hello VAT Declaration"
-            this_dec.company = filters.company
-            this_dec.declaration_year = filters.year
-            this_dec.declaration_month = filters.month
-            this_dec.declaration_month = filters.month
-            this_dec.posting_date = "2020-07-17"
-            this_dec.declaration_items = [
-                {
-                    "link_doctype": "Purchase Invoice",
-                    "link_name": "ACC-PINV-2020-00001"
-                }
-            ]
-            this_dec.save()
+        # frappe.msgprint((str(company) + str(year) + str(month) + str(declared)))
+        if declared == 'Not Declared':
+            vat_dec = frappe.get_doc({
+                    "doctype": "VAT Declaration",
+                    "title": "Hello VAT Declaration",
+                    "company": company,
+                    "posting_date": "2020-07-17",
+                    "declaration_year": year,
+                    "declaration_month": 7,
+                    "declaration_items": [
+                        {
+                            "link_doctype": "Purchase Invoice",
+                            "link_name": "ACC-PINV-2020-00001"
+                        }
+                    ],
+                    "docstatus": 0
+                })
+                # for validated documents: status_journal = vat_dec.insert(ignore_permissions=True)
+            status_declaration = vat_dec.save(ignore_permissions=True)
+
             # Se agrega al vat declararion el item de cada factura presente en esta declaracion
             # Al guardar se agrega a cada una de las facturas el Doctype, Doctype ID o title en el campo de child table Dynamic Link
             # A cada factura de las que tocamos, le agregamos al campo custom field, el titulo de ESTA VAT Declaration que creamos.
         else:
             # We somehow tried to send the declared or all non declared and declared documents.  This is not allowed for our purpose!
-            pass
+           pass
     except:
-        frappe.msgprint("algo salio mal")
+        frappe.msgprint("algo salio mal" + str(frappe.get_traceback()))
+
     else:
-        frappe.msgprint("estas fregado")
+        frappe.msgprint("no estas fregado")
+
 
