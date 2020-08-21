2ae2b5acf91b8fc810a7fe9605a6d34bb69e77e4
monroy95 <m.monroyc22@gmail.com>
2020-08-21 05:48:53 +0000

    FIX no data error, in reporte vat_payable_and_receivable_conciliation

--- factura_electronica/factura_electronica/report/vat_payable_and_receivable_conciliation/vat_payable_and_receivable_conciliation.py
+++ factura_electronica/factura_electronica/report/vat_payable_and_receivable_conciliation/vat_payable_and_receivable_conciliation.py
@@ -157,26 +157,31 @@ def get_data(filters):
     # en_US: Getting the transactions for vat payable accounts to insert the rows, for this month.
     # es: Obtenemos las transacciones de IVA por pagar de este mes para insertar las filas.
 
-    por_pagar = apply_on_site_links(get_vat_payable_data(filters))
-    data.extend(por_pagar)
+    payable_data = get_vat_payable_data(filters)
+    if len(payable_data) > 0:
+        por_pagar = apply_on_site_links(payable_data)
+        data.extend(por_pagar)
 
-    data.append(por_pagar_footer)
-    data.append(empty_row)
-    data.append(empty_row)
-    data.append(por_cobrar_header)
+        data.append(por_pagar_footer)
+        data.append(empty_row)
+        data.append(empty_row)
+        data.append(por_cobrar_header)
 
 
     # en_US: Getting the transactions for vat receivable accounts to insert the rows, for this month.
     # es: Obtenemos las transacciones de IVA por cobrar de este mes para insertar las filas.
-    por_cobrar = apply_on_site_links(get_vat_receivable_data(filters))
-    data.extend(por_cobrar)
+    receivable_data = get_vat_receivable_data(filters)
+    if len(receivable_data) > 0:
+        por_cobrar = apply_on_site_links(receivable_data)
+        data.extend(por_cobrar)
 
-    data.append(por_cobrar_footer)
-    data.append(empty_row)
-    data.append(empty_row)
-    data.append(payable_vat_this_month)
-    data.append(empty_row)
-    data.append(total_vat_payable_now)
+        data.append(por_cobrar_footer)
+        data.append(empty_row)
+        data.append(empty_row)
+        data.append(payable_vat_this_month)
+        data.append(empty_row)
+        data.append(total_vat_payable_now)
+
     return data
 
 # Generate links to documents found.
