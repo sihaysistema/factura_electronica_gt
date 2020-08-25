ba6d7d615ba3560453cd9879542df8aadaefe00d
monroy95 <m.monroyc22@gmail.com>
2020-08-25 22:25:55 +0000

    #178 Traducciones faltantes

--- factura_electronica/fixtures.py
+++ factura_electronica/fixtures.py
@@ -382,7 +382,12 @@ def fill_fixtures():
                     "If you wish to generate a new Journal Entry for this invoice, please cancel the existing policies that reference this invoice",
                     "Sorry, there is already a Journal Entry referenced to this invoice",
                     "More details in the following log",
-                    "Sorry, a problem occurred while trying to generate the Journal Entry"
+                    "Sorry, a problem occurred while trying to generate the Journal Entry",
+                    "Exporter or Supplier", "Importer or Receiver", "Declarant", "Carrier", "Total values",
+                    "Goods", "Support Documents", "Observations and Signatures", "DUCA Number", "Acceptance Date",
+                    "Reference or Serial Number", "Exit Customs Office", "Entry Customs Office", "Initial Transit Customs Office",
+                    "Purpose", "Exporter Identification Type and Number", "Exporter ID number", "Exporter ID type",
+                    "Exporter ID issuing country"
                 ]
             ]
         ]
