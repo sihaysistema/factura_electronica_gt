34a87a878d2db84d8597e0cfdc8a750a2fabc7d2
Tropicalrambler (Alain Berger) <Tropicalrambler@users.noreply.github.com>
2020-08-27 00:41:44 -0600

    adding the function to be called from print formats to Jinja environment

--- factura_electronica/hooks.py
+++ factura_electronica/hooks.py
@@ -35,6 +35,12 @@ app_include_js = "/assets/factura_electronica/js/facelec.min.js"
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
 
+jenv = {
+    "methods": [
+        'currency_in_words:factura_electronica.currency_in_words'
+    ]
+}
+
 # Home Pages
 # ----------
 
