2cf23a0169970d5b7dd4519a3fa1e4f7c3dce468
monroy95 <m.monroyc22@gmail.com>
2021-12-20 19:52:23 +0000

    Cambio version, revision

--- factura_electronica/__init__.py
+++ factura_electronica/__init__.py
@@ -11,7 +11,7 @@ from numinwords import numinwords
 
 # locale.setlocale(locale.LC_ALL, str('en_US.UTF-8'))
 
-__version__ = '7.1.6'
+__version__ = '7.1.7'
 
 @frappe.whitelist()
 def currency_in_words(amount, currency, cent_in_numb=0):
